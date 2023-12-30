1cf2fd760f1ee684704e90a0e887315c62cc69b7
ChickenTM <63499690+ChickenTM@users.noreply.github.com>
2023-12-30 04:35:32 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,151 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <ESP32Servo.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+#define DHT_PIN 15   // DHT11 sensor data pin
+#define SERVO_PIN 13 // Servo motor control pin
+
+DHT dht(DHT_PIN, DHT11);
+Servo myServo;
+WebServer server(80);
+
+const char *ssid = "Mithun";
+const char *password = "drowssap";
+
+float readDHTTemperature();
+float readDHTHumidity();
+
+void handleRoot() // HTML ver 1
+{
+  char msg[1500];
+
+  snprintf(msg, 1500,
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='4'/>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>\
+    <title>ESP32 DHT Server</title>\
+    <style>\
+    html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}\
+    h2 { font-size: 3.0rem; }\
+    p { font-size: 3.0rem; }\
+    .units { font-size: 1.2rem; }\
+    .dht-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}\
+    </style>\
+  </head>\
+  <body>\
+      <h2>Smart Air Vents</h2>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>Temperature</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-tint' style='color:#00add6;'></i>\
+        <span class='dht-labels'>Humidity</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&percnt;</sup>\
+      </p>\
+  </body>\
+</html>",
+           readDHTTemperature(), readDHTHumidity());
+  server.send(200, "text/html", msg);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  myServo.setPeriodHertz(50);
+  myServo.attach(SERVO_PIN, 500, 2500);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("smartairvents"))
+  {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop()
+{
+  server.handleClient();
+  delay(2000);
+
+  float temperature = dht.readTemperature();
+
+  if (!isnan(temperature))
+  {
+    // Serial.print("Temperature: ");
+    // Serial.println(temperature);
+
+    int servoAngle = map(temperature, 25, 29, 0, 180);
+
+    // Serial.print("Servo Angle: ");
+    // Serial.println(servoAngle);
+    Serial.println("Its working.....");
+    myServo.write(servoAngle);
+  }
+  else
+  {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+}
+
+float readDHTTemperature()
+{
+  // Sensor readings may also be up to 2 seconds
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  if (isnan(t))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else
+  {
+    // Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity()
+{
+  // Sensor readings may also be up to 2 seconds
+  float h = dht.readHumidity();
+  if (isnan(h))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else
+  {
+    // Serial.println(h);
+    return h;
+  }
+}
