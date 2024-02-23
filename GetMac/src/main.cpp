a739d833f952026994e4e8fdde6d3820e736bc7f
ChickenTM <63499690+mithunm1@users.noreply.github.com>
2024-02-23 02:35:21 -0500

    ESP-Now

---
+++ GetMac/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop()
+{
+}
